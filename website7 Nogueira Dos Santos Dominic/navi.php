984819f53dc527e934a8bf4b933aaa56fee3f8a1
DominicDosSantosNogueira <NogDo740@school.lu>
2023-10-09 19:17:49 +0200

    navbar prep

---
+++ website7 Nogueira Dos Santos Dominic/navi.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div style="overflow:auto">
+  <ul>
+      <li><a href="Content/About.php">About</a></li>
+      <li><a href="Content/Contact.php">Contact</a></li>
+      <li><a href="Content/Products.php">Products</a></li>
+      <li><a href="Content/Members.php">Members</a></li>
+    </ul>
+
+</body>
+</html>
