6d280ae04cd0163e7a4e695be27c50fa3576e4c1
DominicDosSantosNogueira <NogDo740@school.lu>
2023-10-09 19:04:18 +0200

    php

---
+++ website7 Nogueira Dos Santos Dominic/Content/Sur.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="../Media/Homecss.css">
+</head>
+<body style="font-family:Verdana;color:#0c0b0b;">
+ 
+  <div style="background-color:#e5e5e5;padding:15px;text-align:center;"class="flex-container">
+    <h1><a href="https://maison-orientation.public.lu/de/etudes/portes-ouvertes-des-lycees-luxembourg/ecoles-privees-luxembourg/lpem.html"><img src="../Media/Emile metz icon.png" width="150vw"></a></h1>
+    <div>
+    <h1><a href="../FrenchHome.php"> Transformationmarket</a></h1>
+    <h1>Sur</h1>
+  </div>
+
+  <h1><a href="../Content/About.php"><img src="../Media/England.png" width="150vw"></a></h1>
+    
+      
+    
+  
+  </div>
+
+<div style="overflow:auto">
+  <ul>
+    <li><a href="Sur.php">Sur</a></li>
+      <li><a href="Contctfr.php">Contact</a></li>
+      <li><a href="Produits.php">Produits</a></li>
+      <li><a href="Membres.php">Membres</a></li>
+    </ul>
+
+  <div class="main">
+    <h2>Transformation Market</h2>
+    <p>Dans cette entreprise nous vous vendons la plupart des nouvelles transformations réalisables.</p>
+    <h3>Mises a jours</h3>
+    <p>Sachez que certaines transformations obsolètes peuvent quitter le marché de vente et que de nouvelles peuvent être ajoutées.</p>
+  </div>
+
+  <div class="right">
+    <h2>Ultra Instinct</h2>
+    <img src="../Media/UI.png" width="500vw"> 
+  </div>
+</div>
+
+
+<footer>
+  <p>HTML Nogueira Dos Santos Dominic 2022</p>
+</footer>
+
+</body>
+
+</html>
